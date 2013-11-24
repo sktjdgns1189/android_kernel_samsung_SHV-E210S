9f80b5fc752ad86bfcd5ebdcfad5e135eaee41f4
Sunghun Ra <sktjdgns1189@naver.com>
2013-11-24 20:36:15 +0900

    N5110 Kernel ported to Galaxy S3 by SH

---
+++ Merge-Tool/make.sh
@@ -0,0 +1,5 @@
+cp -rf ../arch/arm/boot/zImage ./
+rm ramdisk-new.gz
+rm boot.img
+./mkbootfs ./ramdisk | gzip > ramdisk-new.gz
+./mkbootimg --kernel zImage --ramdisk ramdisk-new.gz --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o boot.img
---
+++ make_SH.sh
@@ -0,0 +1,12 @@
+#################################
+# mali r3p1 Galaxy S3 Kernel    #
+# ported by Sunghun.Ra [Say]    #
+#################################
+make say_defconfig
+make -j8
+echo :
+echo :
+echo :
+echo If you want make a boot image, use Merge-Tool folder.
+echo Modules : modules.sh
+echo by Sunghun.Ra
